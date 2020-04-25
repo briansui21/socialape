9996e548a262887724d41eddcf206851e79a2ae4
Brian Sui <briansui21@gmail.com>
2020-04-25 12:37:10 -0500

    Initial commit with signup, login, screams

---
+++ socialape-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2020-04-19T23:08:08.786Z',
+      likeCount: 5,
+      commentCount: 2
+    },
+  ],
+};
