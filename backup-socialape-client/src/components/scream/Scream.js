8fdb6bef5217f6724637bfb9e58e0cd7f7728cf9
Brian Sui <briansui21@gmail.com>
2020-05-02 23:32:59 -0500

    Changed default axios url

---
+++ backup-socialape-client/src/components/scream/Scream.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import withStyles from '@material-ui/core/styles/withStyles';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import PropTypes from 'prop-types';
+import MyButton from '../../util/MyButton';
+import DeleteScream from './DeleteScream';
+import ScreamDialog from './ScreamDialog';
+import LikeButton from './LikeButton';
+//MUI Components
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+//Icons
+import ChatIcon from '@material-ui/icons/Chat';
+// Redux
+import { useSelector } from 'react-redux';
+
+const styles = {
+  card: {
+    position: 'relative',
+    display: 'flex',
+    marginBottom: 20,
+  },
+  image: {
+    minWidth: 200,
+  },
+  content: {
+    padding: 25,
+    objectFit: 'cover',
+  },
+};
+
+const Scream = (props) => {
+  dayjs.extend(relativeTime);
+  const {
+    classes,
+    scream: {
+      body,
+      createdAt,
+      userImage,
+      userHandle,
+      screamId,
+      likeCount,
+      commentCount,
+    },
+  } = props;
+  const user = useSelector((state) => state.user);
+
+  const deleteButton =
+    user.authenticated && userHandle === user.credentials.handle ? (
+      <DeleteScream screamId={screamId} />
+    ) : null;
+
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        image={userImage}
+        title="Profile image"
+        className={classes.image}
+      />
+      <CardContent className={classes.content}>
+        <Typography
+          variant="h5"
+          component={Link}
+          to={`/users/${userHandle}`}
+          color="primary"
+        >
+          {userHandle}
+        </Typography>
+        {deleteButton}
+        <Typography variant="body2" color="textSecondary">
+          {dayjs(createdAt).fromNow()}
+        </Typography>
+        <Typography variant="body1">{body}</Typography>
+        <LikeButton screamId={screamId} />
+        <span>{likeCount} Likes</span>
+        <MyButton tip="comments">
+          <ChatIcon color="primary" />
+        </MyButton>
+        <span>{commentCount} Comments</span>
+        <ScreamDialog screamId={screamId} userHandle={userHandle} />
+      </CardContent>
+    </Card>
+  );
+};
+
+Scream.propTypes = {
+  scream: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Scream);
